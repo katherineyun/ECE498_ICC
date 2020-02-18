f8118298914a2d435ee81dfc94504b6d664f6333
katherineyun <32107625+katherineyun@users.noreply.github.com>
2020-02-17 21:50:35 -0600

    Delete trainStep.py

--- trainStep.py
+++
@@ -1,7 +0,0 @@
-"""
-5. trainStep.function : function that implements the training step
-   Provide a file trainStep.py with "function" implemented within it
-
-"""
-def function(session, optimizer):
-    session.run(optimizer)
